b4f51428fdc5ecd19922ea0eb8c85e3a91b65dbc
flortalavera <florii.talavera@gmail.com>
2023-06-16 20:01:08 -0300

    terminada la parte del plan contempo

--- src/app/partials/plan-contempo/plan-contempo.component.ts
+++ src/app/partials/plan-contempo/plan-contempo.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./plan-contempo.component.scss']
 })
 export class PlanContempoComponent {
+  plans: any = {};
 
+  ngOnInit() {
+    this.plans = {
+      "title": ['PLAN 1', 'PLAN 2', 'PLAN 3'],
+      "price": ['$15', '$25', '$29'],
+      "image": [
+        'assets/images/botella.png',
+        'assets/images/botellasx3.png',
+        'assets/images/botellasx2.png'
+      ]
+    };
+  }
 }
